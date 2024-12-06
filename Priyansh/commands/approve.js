5a10674ba824d2467e7c05899a21abb9c0de5a1c
gojokaisen <akiroshota1@gmail.com>
2024-12-06 22:13:40 +0200

    Update approve.js

--- Priyansh/commands/approve.js
+++ Priyansh/commands/approve.js
@@ -1,141 +1,123 @@
 module.exports.config = {
-	name: "approve",
-	version: "1.0.2",
-	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "approve the gc using bots xD",
-	commandCategory: "Admin",
+    name: "approve",
+    version: "1.1.0",
+    hasPermssion: 2,
+    credits: "Frank Kaumba",
+    description: "Manage group chat approvals",
+    commandCategory: "Admin",
     cooldowns: 5
 };
 
-
-const dataPath = __dirname + "/Priyanshu/approvedThreads.json";
-const dataPending = __dirname + "/Priyanshu/pendingdThreads.json";
 const fs = require("fs");
+const path = require("path");
 
+const DATA_FOLDER = path.join(__dirname, "data");
+const APPROVED_PATH = path.join(DATA_FOLDER, "approvedThreads.json");
+const PENDING_PATH = path.join(DATA_FOLDER, "pendingThreads.json");
+
+// Ensure data folder and files exist
 module.exports.onLoad = () => {
-	if (!fs.existsSync(dataPath)) fs.writeFileSync(dataPath, JSON.stringify([]));
-  if (!fs.existsSync(dataPending)) fs.writeFileSync(dataPending, JSON.stringify([]));
+    if (!fs.existsSync(DATA_FOLDER)) fs.mkdirSync(DATA_FOLDER);
+    if (!fs.existsSync(APPROVED_PATH)) fs.writeFileSync(APPROVED_PATH, JSON.stringify([]));
+    if (!fs.existsSync(PENDING_PATH)) fs.writeFileSync(PENDING_PATH, JSON.stringify([]));
 }
-module.exports.handleReply = async function ({ event, api, Currencies, handleReply, Users, args }) {
-    if (handleReply.author != event.senderID) return;
-    const { body, threadID, messageID, senderID } = event;
-    const { type } = handleReply;
-    let data = JSON.parse(fs.readFileSync(dataPath));
-    let dataP = JSON.parse(fs.readFileSync(dataPending));
-    let idBox = (args[0]) ? args[0] : threadID;
-  switch (type) {
-        case "pending": {
-          switch (body) {
-                case `A`: {
-   			data.push(idBox);
-   			fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-   			api.sendMessage(`» Successfully approved the box:\n${idBox}`, threadID, () => {
-          dataP.splice(dataP.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPending, JSON.stringify(dataP, null, 2));
-    	}, messageID)
+
+module.exports.handleReply = async ({ event, api, handleReply }) => {
+    if (handleReply.author !== event.senderID) return;
+
+    const { body, threadID, messageID } = event;
+    const approvedThreads = JSON.parse(fs.readFileSync(APPROVED_PATH));
+    const pendingThreads = JSON.parse(fs.readFileSync(PENDING_PATH));
+
+    if (handleReply.type === "pending" && body.toUpperCase() === "A") {
+        const idBox = handleReply.threadId;
+        
+        approvedThreads.push(idBox);
+        fs.writeFileSync(APPROVED_PATH, JSON.stringify(approvedThreads, null, 2));
+
+        const index = pendingThreads.indexOf(idBox);
+        if (index > -1) {
+            pendingThreads.splice(index, 1);
+            fs.writeFileSync(PENDING_PATH, JSON.stringify(pendingThreads, null, 2));
         }
-        }
-      }
+
+        api.sendMessage(`✅ Successfully approved group: ${idBox}`, threadID, messageID);
     }
-  }
-module.exports.run = async ({ event, api, args, Threads, handleReply, Users }) => {
-	const { threadID, messageID, senderID } = event;
-	let data = JSON.parse(fs.readFileSync(dataPath));
-  let dataP = JSON.parse(fs.readFileSync(dataPending));
-  let msg = "";
-  var lydo = args.splice(2).join(" ");
-  let idBox = (args[0]) ? args[0] : threadID;
-        if (args[0] == "list" || args[0] == "l") {
-    	msg = `=====「 GC THAT HAD BEEN APPROVED: ${data.length} 」 ====`;
-    	let count = 0;
-    	for (e of data) {
-        let threadInfo = await api.getThreadInfo(e);
-          let threadName = threadInfo.threadName ? threadInfo.threadName : await Users.getNameUser(e);
-    		msg += `\n〘${count+=1}〙» ${threadName}\n${e}`;
-    	}
-    	api.sendMessage(msg, threadID, (error, info) => {
-        global.client.handleReply.push({
-            name: this.config.name,
-            messageID: info.messageID,
-            author: event.senderID,
-            type: "a",
-        })
-    }, messageID);
+}
+
+module.exports.run = async ({ event, api, args, Threads, Users }) => {
+    const { threadID, messageID } = event;
+    const approvedThreads = JSON.parse(fs.readFileSync(APPROVED_PATH));
+    const pendingThreads = JSON.parse(fs.readFileSync(PENDING_PATH));
+
+    const prefix = global.config.PREFIX;
+    const subCommand = args[0]?.toLowerCase();
+    const targetId = args[1] || threadID;
+
+    const createThreadListMessage = async (threads, title) => {
+        if (threads.length === 0) return `${title}\nNo threads found.`;
+
+        let msg = `${title}: ${threads.length}\n`;
+        for (let [index, threadId] of threads.entries()) {
+            try {
+                const threadInfo = await api.getThreadInfo(threadId);
+                msg += `${index + 1}. ${threadInfo.threadName || 'Unknown Group'} (${threadId})\n`;
+            } catch (error) {
+                msg += `${index + 1}. Unable to fetch group info (${threadId})\n`;
+            }
         }
-     else if (args[0] == "pending" || args[0] == "p") {
-    	msg = `=====「 THREADS NEED TO BE APPROVE: ${dataP.length} 」 ====`;
-    	let count = 0;
-    	for (e of dataP) {
-        let threadInfo = await api.getThreadInfo(e);
-          let threadName = threadInfo.threadName ? threadInfo.threadName : await Users.getNameUser(e);
-    		msg += `\n〘${count+=1}〙» ${threadName}\n${e}`;
-    	}
-    	api.sendMessage(msg, threadID, (error, info) => {
-        global.client.handleReply.push({
-            name: this.config.name,
-            messageID: info.messageID,
-            author: event.senderID,
-            type: "pending",
-        })
-    }, messageID);
-     }
-       else if (args[0] == "help" || args[0] == "h") {
-         const tst = (await Threads.getData(String(event.threadID))).data || {};
-  const pb = (tst.hasOwnProperty("PREFIX")) ? tst.PREFIX : global.config.PREFIX;
-  const nmdl = this.config.name
-  const cre = this.config.credits
-        return api.sendMessage(`=====「 APPROVE 」=====\n\n${pb}${nmdl} l/list => see list of approved boxes\n\n${pb}${nmdl} p/pending => see the list of unapproved boxes\n\n${pb}${nmdl} d/del => with ID to remove from bot used list\n\n${pb}${nmdl} => Attach an ID to browse that box\n\n⇒ ${cre} ⇐`, threadID, messageID);
-       }
-      
-    else if (args[0] == "del" || args[0] == "d") {
-    	idBox = (args[1]) ? args[1] : event.threadID;
-      if (isNaN(parseInt(idBox))) return api.sendMessage("[ ERR ] Not a number", threadID, messageID);
-    	if (!data.includes(idBox)) return api.sendMessage("[ ERR ] Box is not pre-approved!", threadID, messageID);
-      api.sendMessage(`[ OK ] Your group has been removed from the browsing list by the admin for the reason: ${lydo}`, idBox);
-    	api.sendMessage(`[ OK ] Box has been removed from the list of allowed bots`, threadID, () => {
-    		data.splice(data.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-    	}, messageID)
+        return msg;
+    };
+
+    switch (subCommand) {
+        case "list":
+        case "l":
+            const listMsg = await createThreadListMessage(approvedThreads, "📋 Approved Groups");
+            api.sendMessage(listMsg, threadID, messageID);
+            break;
+
+        case "pending":
+        case "p":
+            const pendingMsg = await createThreadListMessage(pendingThreads, "⏳ Pending Approval Groups");
+            api.sendMessage(pendingMsg, threadID, messageID);
+            break;
+
+        case "del":
+        case "d":
+            if (!approvedThreads.includes(targetId)) 
+                return api.sendMessage("❌ This group is not approved.", threadID, messageID);
+            
+            const index = approvedThreads.indexOf(targetId);
+            approvedThreads.splice(index, 1);
+            fs.writeFileSync(APPROVED_PATH, JSON.stringify(approvedThreads, null, 2));
+
+            api.sendMessage(`🗑️ Removed group ${targetId} from approved list.`, threadID, messageID);
+            break;
+
+        case "help":
+        case "h":
+            const helpMessage = `
+📌 Approve Command Help:
+- ${prefix}approve list/l - View approved groups
+- ${prefix}approve pending/p - View pending groups
+- ${prefix}approve del/d [groupID] - Remove group from approved list
+- ${prefix}approve [groupID] - Approve a specific group
+            `;
+            api.sendMessage(helpMessage, threadID, messageID);
+            break;
+
+        default:
+            if (isNaN(targetId)) 
+                return api.sendMessage("❌ Invalid group ID.", threadID, messageID);
+
+            if (approvedThreads.includes(targetId)) 
+                return api.sendMessage(`✅ Group ${targetId} is already approved.`, threadID, messageID);
+
+            // Add to approved threads
+            approvedThreads.push(targetId);
+            fs.writeFileSync(APPROVED_PATH, JSON.stringify(approvedThreads, null, 2));
+
+            api.sendMessage(`🎉 Group ${targetId} has been approved!`, threadID, messageID);
+            break;
     }
-    else if (isNaN(parseInt(idBox))) api.sendMessage("[ ERR ] The ID you entered is not valid", threadID, messageID);
-    else if (data.includes(idBox)) api.sendMessage(`[ - ] ID ${idBox} pre-approved!`, threadID, messageID);
-   	else api.sendMessage("[ OK ] ✨ApKa Group Approved Kar Diya Hai🙌.\n 🖤So Enjoy\n\n 💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞𝕻𝖗𝖎𝖞𝖆𝖓𝖘𝖍 𝕽𝖆𝖏𝖕𝖚𝖙☜ 💫\n 🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ www.facebook.com/profile.php?id=100037743553265\n👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇", idBox, (error, info) => {
-   		api.changeNickname(` 〖 ${global.config.PREFIX} 〗 ➺ ${(!global.config.BOTNAME) ? "" : global.config.BOTNAME}`, idBox, global.data.botID);
-      const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-   let admID = "100037743553265";    
-  
-      api.getUserInfo(parseInt(admID), (err, data) => {
-      if(err){ return console.log(err)}
-     var obj = Object.keys(data);
-    var firstname = data[obj].name.replace("@", "");  
-      
-      axios.get('https://anime.apibypriyansh.repl.co/img/anime').then(res => {
-	let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
-	let callback = function () {
-      api.sendMessage({body: `❒❒ BOT ARE NOW CONNECTED ❒❒\n=====================\n┏━━━━ 🖤 ━━━━┓
-  ✦❥⋆⃝ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙 ✦ 
-┗━━━    🖤 ━━━━┛\n=====================\n➪ BOT: ${global.config.BOTNAME}\n➪ Prefix: ${global.config.PREFIX}\n➪ Users: ${global.data.allUserID.length}\n➪ Groups: ${global.data.allThreadID.length}\n=====================\n[]---------------------------------------[]\nUse '${global.config.PREFIX}Help' T0o View The Commands That Available!(ღ˘⌣˘ღ)\n[]---------------------------------------[]\n⌨ Made by: ${firstname}\n`, mentions: [{
-                           tag: firstname,
-                           id: admID,
-                           fromIndex: 0,
-                 }],
-						attachment: fs.createReadStream(__dirname + `/cache/duyet.${ext}`)
-					}, idBox,() => fs.unlinkSync(__dirname + `/cache/duyet.${ext}`));
-				};
-				request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/duyet.${ext}`)).on("close", callback);
-			}) 
-      })
-   		if (error) return api.sendMessage("[ ERR ] Something went wrong, make sure the id you entered is valid and the bot is in the box!", threadID, messageID);
-   		else {
-   			data.push(idBox);
-   			fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-   			api.sendMessage(`[ OK ] Successfully Approved The Box (◕‿◕):\n${idBox}`, threadID, () => {
-          dataP.splice(dataP.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPending, JSON.stringify(dataP, null, 2));
-    	}, messageID)
-        }
-   	});
-  }
+}
